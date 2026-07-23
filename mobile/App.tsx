69e8348201de490727886f418b1097100b79ca1a
tanaka-prince49 <tanakaprince49@gmail.com>
2026-07-23 13:28:44 +0200

    move tab bar further down

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -282,7 +282,7 @@ function TabNavigator({ navigation }: any) {
           position: 'absolute',
           left: 16,
           right: 16,
-          bottom: insets.bottom + 26,
+          bottom: insets.bottom + 70,
           height: 66 + insets.bottom,
           borderRadius: 20,
           backgroundColor: isDark ? COLORS.darkBgSec : COLORS.lightBgSec,
